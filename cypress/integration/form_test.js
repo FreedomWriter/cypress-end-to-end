bc7ce3de7c232083ef77cd793a79da174aadb4dc
FreedomWriter <natalienetworks@gmail.com>
2020-03-26 12:32:46 -0500

    form tested

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -1,6 +1,27 @@
-describe("Testing form", function() {
+/* eslint-disable no-undef */
+describe("Testing form", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000/");
   });
-  it("Add text to inputs and submit form", function() {});
+  it("Add text to inputs and submit form", () => {
+    cy.get('input[name="name"]')
+      .type("Natalie")
+      .should("have.value", "Natalie");
+
+    cy.get('input[name="email"]')
+      .type("email@email.com")
+      .should("have.value", "email@email.com");
+
+    cy.get("textarea")
+      .type("I want to help people!")
+      .should("have.value", "I want to help people!");
+
+    cy.get("#positions")
+      .select("Yard Work")
+      .should("have.value", "Yard Work");
+    cy.get('[type="checkbox"]')
+      .check()
+      .should("be.checked");
+    cy.get("button").click();
+  });
 });
--- notes.md
+++ notes.md
@@ -2,10 +2,14 @@
 
 Cypress is an e2e testing library with a GUI
 [Docs]("https://docs.cypress.io")
+[Assertions]("https://docs.cypress.io/guides/core-concepts/introduction-to-cypress.html#Assertions")
 
 ## Dependencies
 
 - `npm i cypress --save -dev`
 - `npx cypress open`
 
-## Scripts
+## Run Tests Without GUI
+
+- `npx cypress run --spec "cypress/integration/form_test.js"` to run a specific test file
+- `npx cypress run` runs all test`
